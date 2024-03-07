0b33dfd959e383a639c2a88edeb9b8c197aeb074
vamuse <vamusedam@gmail.com>
2024-03-07 01:39:03 -0300

    refactor(env): :fire: remove unnecesary functions and schema properties

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,27 +2,12 @@ import { z } from 'zod'
 
 const envSchema = z.object({
 	DATABASE_URL: z.string(),
-	DISCORD_TOKEN: z.string(),
-	DISCORD_GUILD_ID: z.string(),
-	API_ACCESS_TOKEN: z.string(),
-	PORT: z.string().default('5000'),
+	DATABASE_AUTH_TOKEN: z.string(),
 })
 
-const env = envSchema.parse(process.env)
+envSchema.parse(process.env)
 
 export type Env = z.infer<typeof envSchema>
-
-export function envParseNumber<T extends keyof Env>(variable: T): number {
-	return Number.parseInt(env[variable])
-}
-
-export function envParseBoolean<T extends keyof Env>(variable: T): boolean {
-	return env[variable] === 'true'
-}
-
-export function envParseArray<T extends keyof Env>(variable: T): string[] {
-	return env[variable].split(', ')
-}
 
 declare global {
 	namespace NodeJS {
